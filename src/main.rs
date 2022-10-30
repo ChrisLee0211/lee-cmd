139deeb2f055fcaa0efafcfde43afdc2c1ed5ee9
李铭辉 <1018596741@qq.com>
2022-10-30 12:43:58 +0800

    feat: 新增push指令缩写版

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{Parser, Subcommand, Args};
-
+use std::process::{Command};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -13,7 +13,9 @@ enum Action {
     /// git pull origin xxx
     PL(Branch),
     /// git push origin xxx
-    PH(Branch)
+    PH(Branch),
+    /// remove cargo cache
+    RC
 }
 #[derive(Args, Debug)]
 struct Branch {
@@ -21,16 +23,70 @@ struct Branch {
     branch: Option<String>,
 }
 
+fn excute_git_push(branch_str: &String) {
+    let output = Command::new("git").args(["push", "origin", branch_str]).output();
+    match output {
+        Ok(output_info) => {
+            if !output_info.status.success() {
+                let raw_output = String::from_utf8(output_info.stderr).unwrap();
+                println!("push fail cause by {}", raw_output);
+                return;
+            }
+            println!("push success!!!",);
+        }
+        Err(err) => {
+            panic!("git push fail cause by {}", err);
+        }
+    }
+}
+
+fn git_push(args: &Option<String>) {
+    match args {
+        Some(branch) => {
+            excute_git_push(branch)
+        }
+        None => {
+            let current_branch_ouput = Command::new("git").args(["symbolic-ref", "--short", "HEAD"]).output().ok().unwrap();
+            let branch = String::from_utf8(current_branch_ouput.stdout).unwrap().trim().to_string();
+            excute_git_push(&branch)
+
+        }
+    }
+}
+
+fn git_pull(args: &Option<String>) {
+
+}
+
+fn remove_cargo_cache() {
+    let output = Command::new("rm").args(["-rf", "~/.cargo/.package-cache"]).output();
+    match output {
+        Ok(output_info) => {
+            if(!output_info.status.success()) {
+                let raw_output = String::from_utf8(output_info.stderr).unwrap();
+                println!("remove fail cause by {}", raw_output);
+                return;
+            }
+            println!("remove cargo package cache success")
+        }
+        Err(err) => {
+            panic!("remove fail cause by {}", err);
+        }
+    }
+}
 
 fn main() {
     let cli = Commands::parse();
 
     match &cli.action {
         Action::PH(Branch { branch }) => {
-            println!("git push {:?}", &branch)
+            git_push(&branch)
         }
         Action::PL(Branch { branch }) => {
 
+        }
+        Action::RC => {
+            remove_cargo_cache()
         }
     }
 }
