62bd598685767eb143de0c2b264b011bcc163501
李铭辉 <1018596741@qq.com>
2022-11-01 22:50:11 +0800

    fix: 更新format_branch实现

--- src/actions/git.rs
+++ src/actions/git.rs
@@ -19,7 +19,7 @@ pub fn git_pull(args: &Option<String> ){
 
 pub fn format_branch<'a>(args: &'a Option<String>) -> String {
     match args {
-        Some(branch) => branch.to_owned(),
+        Some(branch) => branch.to_string(),
         None => {
             let current_branch_ouput = Command::new("git")
                 .args(["symbolic-ref", "--short", "HEAD"])
@@ -29,8 +29,8 @@ pub fn format_branch<'a>(args: &'a Option<String>) -> String {
             let branch = String::from_utf8(current_branch_ouput.stdout)
                 .unwrap()
                 .trim()
-                .to_string().to_owned();
-            branch
+                .to_string();
+           branch
         }
     }
 }
